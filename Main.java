e00eaa7e8ee6d21362f22a2fe82d324ca1d55d30
otaku904 <domu904@gmail.com>
2020-10-28 20:56:22 -0500

    inclusive year and valid currency

--- Main.java
+++ Main.java
@@ -71,19 +71,11 @@ class Result {
     }
 
     public static boolean isInclusiveYear(String str) {
-        for (int i = 0; i < str.length(); i++)
-            for (int j = i + 1; j < str.length(); j++)
-                if (str.charAt(i) == str.charAt(j))
-                    return false;
-
+        
         return false;
     }
 
     public static boolean isValidCurrency(String str) {
-        for (int i = 0; i < str.length(); i++)
-            for (int j = i + 1; j < str.length(); j++)
-                if (str.charAt(i) == str.charAt(j))
-                    return false;
 
         return false;
     }
